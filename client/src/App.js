6182f8e9b3e15d01a923f9051536795c70e58b15
Amritanshu Kumar <amritanshu8271@gmail.com>
2024-07-10 09:50:27 +0530

    register aand login adjusted

--- client/src/App.js
+++ client/src/App.js
@@ -7,8 +7,11 @@ import Home from "./pages/home/Home";
 import Hotel from "./pages/hotel/Hotel";
 import List from "./pages/list/List";
 import Login from "./pages/login/Login";
+import New from "./pages/new/New";
+import { userInputs } from "./formSource.js";
 
 function App() {
+  console.log(userInputs);
   return (
     <BrowserRouter>
       <Routes>
@@ -16,6 +19,7 @@ function App() {
         <Route path="/hotels" element={<List/>}/>
         <Route path="/hotels/:id" element={<Hotel/>}/>
         <Route path="/login" element={<Login/>}/>
+        <Route path="/New" element= {<New inputs={userInputs} />}/>
       </Routes>
     </BrowserRouter>
   );
--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -27,7 +27,8 @@ const Login=()=>{
             dispatch({type:"LOGIN_SUCCESS",payload: res.data.details});
             navigate("/");
         } catch (err) {
-            dispatch({type:"LOGIN_FAILURE", payload: err.response.data})
+            dispatch({type:"LOGIN_FAILURE", payload: err.response.data});
+            console.log(err);
         }
     }
 
